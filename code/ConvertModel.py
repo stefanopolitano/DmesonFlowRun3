f8ca7d39c02137f6a5d62a96fc2123491526c298
stefanopolitano <stefano.politano@cern.ch>
2024-03-06 11:46:04 +0100

    Adding onnx conversion

--- code/ConvertModel.py
+++ code/ConvertModel.py
@@ -3,27 +3,24 @@ python script to convert ML models from .pkl to .model
 '''
 
 import os
-import sys
 import argparse
 
 from hipe4ml.model_handler import ModelHandler
+from hipe4ml_converter.h4ml_converter import H4MLConverter
 
 parser = argparse.ArgumentParser(description='Arguments to pass')
 parser.add_argument('inFilePkl', metavar='text', default='model.pkl', help='input pickle file to be converted')
+parser.add_argument('nfeatures', metavar='int', default=10, help='number of features')
 args = parser.parse_args()
 
 ModelPath = os.path.expanduser(args.inFilePkl)
 print(f'Loaded saved model: {ModelPath}')
 ModelHandl = ModelHandler()
 ModelHandl.load_model_handler(ModelPath)
+model_converter = H4MLConverter(ModelHandl) # create the converter object
+model_onnx = model_converter.convert_model_onnx(1, args.nfeatures) # convert the model to ONNX format
 
-if '.pickle' in ModelPath:
-    outFileName = ModelPath.replace('.pickle', '.model')
-elif '.pkl' in ModelPath:
-    outFileName = ModelPath.replace('.pkl', '.model')
-else:
-    print(f'ERROR: invalid input file {ModelHandl}, please check it! Exit')
-    sys.exit()
+outFileName = ModelPath.replace('.pkl', '.onnx')
 
-ModelHandl.dump_original_model(outFileName, True)
+model_converter.dump_model_onnx(model_onnx, outFileName) # save the model to a file
 print(f'Saved model: {outFileName}')
